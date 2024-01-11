ced918eb09e34e4fff0b5e27bcfbb44bd7210c98
Astdongjin <djpppark5@gmail.com>
2024-01-11 16:10:34 +0900

    이 것 은 첫 번 째 pwm프 로 젝 트 임

---
+++ Core/Inc/uart.h
@@ -0,0 +1,18 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: djppp
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "main.h"
+
+
+// 변수 프로토타입 선언
+void initUart(UART_HandleTypeDef *);   // UART 장치 초기화
+
+
+#endif /* INC_UART_H_ */
